aa77859ba4c1cc22be015169ff9eecb53fcc9345
HShrigma <hristostantchev@abv.bg>
2026-03-04 19:06:04 +0200

    feat(combination): make combinations alternating

--- project/src/utils/combinationGenerator.ts
+++ project/src/utils/combinationGenerator.ts
@@ -1,14 +1,22 @@
 import { DoorCommand, DoorDirection } from "./types/vaultRegistries";
 
-const getRandomCommand = ():DoorCommand =>{
-    const dir = Math.random() > .5 ? DoorDirection.CW : DoorDirection.CCW;
+const getRandomCommand = (dir: DoorDirection | undefined): DoorCommand => {
+    if (dir === undefined)
+        dir = Math.random() > .5 ? DoorDirection.CW : DoorDirection.CCW;
+    else 
+        dir = dir === DoorDirection.CW ? DoorDirection.CCW : DoorDirection.CW;
     const amount = Math.floor(Math.random() * (9) + 1);
     return { direction: dir, amount }
 }
 
 const getRandomCombination = (): DoorCommand[] => {
     let commands = [];
-    for (let index = 0; index < 3; index++) commands.push(getRandomCommand());
+    let lastDirection: DoorDirection | undefined;
+    for (let index = 0; index < 3; index++) {
+        const command = getRandomCommand(lastDirection); 
+        lastDirection = command.direction;
+        commands.push(command);
+    };
     return commands;
 }
 export const CombinationGenerator = {
